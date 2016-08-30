48b9c66df057c5e8e1afbff95d05f07569752fe6
bryanyuen <18yuent89@millshighsch.org>
2016-08-30 11:38:16 -0700

    convert
    convert dF to dC

---
+++ Convert/src/convert.java
@@ -0,0 +1,12 @@
+
+public class convert { 
+	public static void main (String [] args) {
+		//declare variables
+		double dC;
+		double dF = 80;
+		dC = 5.0/9.0* (dF - 32);
+		System.out.println(dF + "degree fahrenheit is ");
+		System.out.println(dC);
+	}
+
+}
