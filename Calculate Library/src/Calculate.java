12bb18beaf0a5b3e844e306c2c7eb9ce292475ee
bryanyuen <18yuent89@millshighsch.org>
2016-10-05 11:23:05 -0700

    10/5

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -1,51 +1,62 @@
 public class Calculate {
+	//This program is used to square the integer.
 	public static int square (int number){
 		return(number*number);
 	}
+	//This program is used to cube the integer.
 	public static int cube (int number){
 		int answer;
 		answer=number*number*number;
 		return(answer);
 	}
-	public static int average(int number){
-		int answer=(number+number)/2;
+	//This program is used to find out the average of two integers.
+	public static int average(int number,int number2){
+		int answer=(number+number2)/2;
 		return(answer);
 	}
-	public static double average(double number){
+	//This program is used to find out the average of three doubles.
+	public static double average(double number, double number2, double number3){
 		double answer;
-		answer=(number+number+number)/3;
+		answer=(number+number2+number3)/3;
 		return(answer);
 	}
+	//This program is used to convert degree to radian.
 	public static double toDegrees (double radian){
 		double answer;
 		answer=radian*(180/3.14159);
 		return(answer);
 	}
+	//This program is used to convert radian to degree.
 	public static double toRadians (double degree){
 		double answer;
 		answer=degree*(3.14159/180);
 		return(answer);
 	}
+	//This program can help you to find the discriminant of three doubles.
 	public static double discriminant (double aValue,double bValue,double cValue){
 		double answer;
 		answer= (bValue*bValue)+(-4*aValue*cValue);
 		return(answer);
 	}
+	//This program is used to convert mixed number to an improper fraction.
 	public static String toImproperFrac (int wholenumber, int numerator, int denominator){
 		String answer;
 		answer= ((wholenumber*denominator)+numerator)+"/"+denominator;
 		return(answer);
 	}
+	//This program is used to convert improper fraction to mixed number.
 	public static String toMixedNum (int numerator, int denominator){
 		String answer;
 		answer=(numerator/denominator)+"_"+(numerator%denominator)+"/"+denominator;
 		return answer;
 	}
-	public static String foil (int a,int b, int c, int d){
+	//This program is used to convert a binomial multiplication form into a quadratic equation form.
+	public static String foil (int a,int b, int c, int d,String character){
 		String answer;
-		answer=(a*c)+"x^2"+((a*d)+(b*c))+"x"+(b*d);
+		answer=(a*c)+ character+"^2 + "+((a*d)+(b*c))+character+" +"+(b*d);
 		return answer;
 	}
+	//This program is used to determine whether or not one integer is evenly divisible by another.
 	public static boolean isDivisibleBy (int a, int b){
 		if(a%b==0){
 			return true;
@@ -53,13 +64,15 @@ public class Calculate {
 			return false;
 		}
 	}
+	//This program is used to return a number to the absolute value.
 	public static double absValue (double a){
 		if(a<0){
-			return (a+a+a);
+			return (a+(-a)+(-a));
 		}else{
 			return a;
 		}
 	}
+	//This program is used to find out the largest integer from the inputs.
 	public static int max (int a, int b){
 		if(a>b){
 			return a;
@@ -67,6 +80,7 @@ public class Calculate {
 			return b;
 		}
 	}
+	//This program is used to find out the largest double from the inputs.
 	public static double max (double a, double b){
 		if(a>b){
 			return a;
@@ -74,6 +88,7 @@ public class Calculate {
 			return b;
 		}
 	}
+	//This program is used to find out the smallest integer from the input.
 	public static int min (int a, int b){
 		if(a>b){
 			return b;
@@ -81,6 +96,7 @@ public class Calculate {
 			return a;
 		}
 	}
+	//This program is used to round a double to 2 decimal places.
 	public static double round2 (double a){
 		int b = (int)(a*100);
 		double x;
@@ -91,19 +107,21 @@ public class Calculate {
 		return x;
 		}
 	}
-	public static double exponent (double a, int b){
+	//This program is used to raise a value to a positive integer power.
+	public static double exponent (double number, int exponent){
 	 int answer =1;
-		for (int i = 1; i<=b; i++){
-			answer *=a;		}
+		for (int i = 1; i<=exponent; i++){
+			answer *=number;		}
 		return answer;
 	}
-	public static int factorial (int a){
-		int answer=1;
-		for (int i = 1; i<=a; i++){
-			answer *= a;
+	//This program is used to the factorial of the value passed.
+	public static int factorial (int number){
+		for (int i = number-1; i > 0; i--){
+			number = number * i;
 		}
-		return answer;
+		return number;
 	}
+	
 	public static boolean isPrime (int a){
 		boolean check;
 		for(int i= a-1; i>1; i--){
@@ -111,7 +129,7 @@ public class Calculate {
 			if (check == true){
 				return false;
 			}
-			}
+		}
 		return true;
 	}
 	public static int gcf (int a, int b){
