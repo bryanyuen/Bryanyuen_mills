4002738c15747622aa900a5c9ccbd8c3642b201d
bryanyuen <bryanyuen@bryandeMacBook-Pro.local>
2016-10-06 20:46:44 -0700

    10/6 late

--- MagPie2/src/Magpie2.java
+++ MagPie2/src/Magpie2.java
@@ -15,11 +15,20 @@ public class Magpie2 {
 		String response = "";
 		if (statement.indexOf("no") >= 0) {
 			response = "Why so negative?";
-		} else if (statement.indexOf("mother") >= 0
+		}if (statement.indexOf("ball") >= 0){
+			response = "You must be sporty!!!";
+		}if (statement.indexOf("lunch") >= 0){
+			response = "I am so hungry!!!";
+		}if (statement.indexOf("teacher") >= 0){
+			response = "He sounds like a good teacher.";
+		}if (statement.indexOf("mother") >= 0
 				|| statement.indexOf("father") >= 0
 				|| statement.indexOf("sister") >= 0
 				|| statement.indexOf("brother") >= 0) {
 			response = "Tell me more about your family.";
+		}
+		if (statement.length() == 0){
+			response = "Say something, please.";
 		} else {
 			response = getRandomResponse();
 		}
@@ -31,7 +40,7 @@ public class Magpie2 {
 	 * returns a non-committal string
 	 */
 	private String getRandomResponse() {
-		final int NUMBER_OF_RESPONSES = 4;
+		final int NUMBER_OF_RESPONSES = 6;
 		double r = Math.random();
 		int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
 		String response = "";
@@ -44,7 +53,12 @@ public class Magpie2 {
 			response = "Do you really think so?";
 		} else if (whichResponse == 3) {
 			response = "You don't say.";
+		}else if (whichResponse == 4){
+			response = "What are you talking about?";
+		}else if (whichResponse == 5){
+			response = "No comment!!";
 		}
+		
 
 		return response;
 	}
